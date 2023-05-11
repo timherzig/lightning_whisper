f16ef238e2288f72b264eaf56023323cb78b2398
Tim Herzig <tim.herzig@hotmail.com>
2023-05-11 15:55:27 +0200

    cleaner dimension def.

--- src/model.py
+++ src/model.py
@@ -30,27 +30,18 @@ class Whisper(pl.LightningModule):
         self.whisper_encoder.config.forces_decoder_ids = None
         self.whisper_encoder._freeze_parameters()
 
-        # self.lin1 = nn.Linear(in_features=self.whisper_encoder.embed_positions.embedding_dim, out_features=1)
-        # self.lin2 = nn.Linear(in_features=1500, out_features=1)
         self.bilstm = nn.LSTM(self.whisper_encoder.embed_positions.embedding_dim, self.config.model.bilstm.hidden_size, bidirectional=True, batch_first=True)
-        self.linear1 = nn.Linear(192000, 256) #self.config.model.bilstm.hidden_size*4, 512)
+        self.linear1 = nn.Linear(2*self.whisper_encoder.embed_positions.num_embeddings*self.bilstm.hidden_size, 256)
         self.linear2 = nn.Linear(256, 1)
         self.sigmoid = nn.Sigmoid()
         
     def forward(self, x, sr=16000):
-        # x = self.processor.feature_extractor(x, sampling_rate=sr, return_tensors='pt').input_features
-        print(f'Before size: {x.size()}')
         out = self.whisper_encoder(x).last_hidden_state
-        print(f'After Whisper Encoder size: {out.size()}')
         out, _ = self.bilstm(out)
         out = torch.flatten(out, start_dim=1)
-        print(f'After BILSTM size: {out.size()}')
         out = self.linear1(out)
-        print(f'After Linear1 size: {out.size()}')
         out = self.linear2(out)
-        print(f'After Linear2 size: {out.size()}')
         out = torch.squeeze(self.sigmoid(out))
-        print(f'After Sigmoid size: {out.size()}')
         return out
 
     def configure_optimizers(self):
